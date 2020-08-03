caf9b3acc9f71df6ecaa537901426d4d84546b02
Nick Wellman <wellman.nicholas@gmail.com>
2020-08-03 17:33:43 -0400

    add model for hwllo world

--- src/main/java/com/lordgasmic/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/lordgasmic/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.lordgasmic.helloworld.controller;
 
+import com.lordgasmic.helloworld.model.HW;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/api/v1/hello/world")
-    public String get(){
-        return "derp";
+    public HW get(){
+        HW hw = new HW();
+        hw.setHello("derpsicile");
+        return hw;
     }
 }
---
+++ src/main/java/com/lordgasmic/helloworld/model/HW.java
@@ -0,0 +1,8 @@
+package com.lordgasmic.helloworld.model;
+
+import lombok.Data;
+
+@Data
+public class HW {
+    private String hello;
+}
